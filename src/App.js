0c7690988c4f0114dd530e1ba44d4ba1ab60f837
Ankit Shrestha <ankit.shrestha196@gmail.com>
2023-04-20 07:26:03 +0545

    Add loader when login page is first loaded

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./App.css"
 import Login from "./components/Login/Login"
 import Successful from "./components/Successful/Successful"
 
 function App() {
   const [isEmptyField, UpdateIsEmpty] = useState(true)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [])
+
   const filledData = (isEmpty) => {
     UpdateIsEmpty(isEmpty)
   }
   return (
     <div className="App">
-      <Login filledData={filledData} />
+      {loading ? (
+        <span className="loader"></span>
+      ) : (
+        <Login filledData={filledData} />
+      )}
+      {/* <Login filledData={filledData} />
 
-      {!isEmptyField ? <Successful /> : <span className="loader"></span>}
+      {!isEmptyField ? <Successful /> : <span className="loader"></span>} */}
     </div>
   )
 }
